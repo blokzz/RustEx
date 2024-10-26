957bc424bfce2bc462cba8472fa2dd49aee2dd94
Kamil <125974589+blokzz@users.noreply.github.com>
2024-10-26 22:56:11 +0200

    Update main.rs

--- Intermediate-level/set1/zadanie6/src/main.rs
+++ Intermediate-level/set1/zadanie6/src/main.rs
@@ -1,13 +1,12 @@
-
-use axum::{routing::get, Router};
+use axum::{routing::{get, post}, Router};
 use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
-    .route("/", get(|| async { "WItaj" }))
-    .route("/hello", get(|| async { "raz dwa trzy" }))
-    .route("/status", get(|| async { "Serwer dziala" }));
+        .route("/", get(|| async { "Witaj" }))
+        .route("/hello", get(|| async { "raz dwa trzy" }))
+        .route("/status", post(|| async { "Serwer działa" }));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     println!("Server is running on http://{}", addr);
